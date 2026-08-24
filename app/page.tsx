9d57526f7eae2378087bfe98702adc8c66fafeec
BuhoElectronics <buhoelectronics@buhoelectronics.com>
2026-08-24 04:43:20 +0000

    remove pnpm

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div style={{ padding: 40 }}>
       <h1>Panel Administrativo Buho Electronics</h1>
-      <p>Tu panel está funcionando correctamente.</p>
+      <p>Tu panel está funcionando exitosamente.</p>
     </div>
   );
 }
